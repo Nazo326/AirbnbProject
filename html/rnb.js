4206b9b8f6b4a7cf37603846029ed868743727c9
Nazo326 <gracesil2019@gmail.com>
2025-08-27 13:18:53 +0100

    wednesday 27th

--- html/rnb.js
+++ html/rnb.js
@@ -1,19 +1,38 @@
 
-  document.addEventListener("DOMContentLoaded", function () {
-    let tabs = document.querySelectorAll(".tab-link");
-    let panels = document.querySelectorAll(".tab-panel");
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   let tabs = document.querySelectorAll(".tab-link");
+  //   let panels = document.querySelectorAll(".tab-panel");
 
-    tabs.forEach(tab => {
-      tab.addEventListener("click", e => {
-        e.preventDefault();
-        // deactivate all
-        tabs.forEach(t => t.classList.remove("active"));
-        panels.forEach(p => p.classList.remove("active"));
-        // activate selected tab + panel
-        tab.classList.add("active");
-        let panel = document.getElementById(tab.dataset.tab);
-        if (panel) panel.classList.add("active");
-      })
-    })
+  //   tabs.forEach(tab => {
+  //     tab.addEventListener("click", e => {
+  //       e.preventDefault();
+  //       // deactivate all
+  //       tabs.forEach(t => t.classList.remove("active"));
+  //       panels.forEach(p => p.classList.remove("active"));
+  //       // activate selected tab + panel
+  //       tab.classList.add("active");
+  //       let panel = document.getElementById(tab.dataset.tab);
+  //       if (panel) panel.classList.add("active");
+  //     })
+  //   })
     
-
+// DROPDOWN MENU
+// buttondropdown for landing page
+    function displayMenu() {
+        let div = document.getElementsByClassName("dropdown")[0];
+        if (div) {
+            // Toggle display between 'block' and 'none'
+            if (div.style.display === "block") {
+                div.style.display = "none";
+                div.style.boxShadow = "";
+            } else {
+                div.style.display = "block";
+                div.style.boxShadow = "0px 0px 8px 0px rgb(224, 224, 224)";
+            }
+        }
+    }
+// Attach the event to a button that should trigger the menu
+    let button = document.querySelector(".menu-button");
+    if (button) {
+        button.onclick = displayMenu;
+    }
